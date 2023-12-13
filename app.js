d93a1c69ecae1042a01aee5568c103a70f91c8de
Henrique <h.sp97@hotmail.com>
2023-12-13 20:45:13 -0300

    added authentication

--- app.js
+++ app.js
@@ -3,22 +3,46 @@ const app = express();
 const methodOverride = require('method-override');
 const path = require('path');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const expressSession = require('express-session');
 const User = require('./models/user');
-
-mongoose.connect('mongodb://127.0.0.1:27017/dbUser').then(() => {
-  console.log("Connection established!");
-}).catch(err => {
-  console.log("Failed to connect!");
-  console.log(err);
-});
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+// Session and passport
+app.use(expressSession({
+  secret: 'my_secret',
+  resave: false,
+  saveUninitiaded: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/dbUser').then(() => {
+  console.log("Connection established!");
+}).catch(err => {
+  console.log("Failed to connect!" + err);
+});
+
+// Routes
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -27,20 +51,31 @@ app.get('/register', (req, res) => {
   res.render('register')
 });
 
-app.post('/register', async (req, res) => {
-  try {
-    const { name, username, password } = req.body;
-    const user = new User({ name, username, password });
-    await user.save();
-    res.redirect('/login');
-  } catch (error) {
-    console.error(error);
-    res.send('Error registering user.');
-  }
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  User.register(new User({username, password }), password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.render('register');
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/dashboard');
+      });
+    }
+  });
 });
 
 app.get('/login' , (req, res) => {
   res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/login'
+}));
+
+app.get('/dashboard', isLoggedIn, (req, res) => {
+  res.render('dasboard');
 });
 
 app.listen(3000, () => {
